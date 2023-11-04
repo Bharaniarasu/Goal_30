151465cbfe4c2e8824b95b0d4aa9bf09147325de
Bharaniarasu <bharaniarasu.t@gmail.com>
2023-11-04 20:45:05 +0530

    gh pages installed

--- javascript_30_navbar/src/App.js
+++ javascript_30_navbar/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.scss";
 import Header from "./components/layouts/Header";
 import Home from "./components/home/Home";
